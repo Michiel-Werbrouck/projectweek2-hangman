32fa3ac7f1414f18f8fc74584ba9fdaeb4ed2113
michieldebruyn <71533210+michieldebruyn@users.noreply.github.com>
2021-03-29 13:49:59 +0200

    FxPuntApp_beautify

--- src/ui/FxPuntApp.java
+++ src/ui/FxPuntApp.java
@@ -11,11 +11,13 @@ public class FxPuntApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         GridPane root = new GridPane();
-        Scene scene = new Scene(root,200,100);
+        Scene scene = new Scene(root,500,350);
         new PuntApp(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Punt App");
         primaryStage.show();
+        root.setLayoutX(10);
+        root.setLayoutY(10);
     }
 
     public static void main(String[] args) {
